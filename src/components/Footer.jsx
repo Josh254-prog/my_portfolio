b21939e413ca0ae60141bf98d020fbdaf60fd3e3
Joshua Opele <joshuaokoth956@gmail.com>
2025-09-08 16:07:28 +0300

    Corrected the spelling mistakes in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer className="bg-primaryDark text-textLight py-8 border-t border-gray-700 text-center">
       <div className="max-w-7xl mx-auto">
         <p>&copy; {new Date().getFullYear()} Joshua Opele. All Rights Reserved.</p>
-        <p>Powered by Moonsoon Industries</p>
+        <p>Powered by Monsoon Industries</p>
       </div>
     </footer>
   );
